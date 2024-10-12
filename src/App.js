332286563face4395f19971d12eb03af2d7cde30
sumiya <urgeljid1@gmail.com>
2024-10-12 19:54:36 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './Frame.css';
+import './global.css'
+const Section = ({ title, description, imgSrc }) => (
+  <div className="section">
+    <div className="image-container">
+      <img className="section-icon" alt="" src={imgSrc} />
+    </div>
+    <div className="text-container">
+      <div className="heading">
+        <p className="title">{title}</p>
+        <p className="description">{description}</p>
+      </div>
+    </div>
+  </div>
+);
 
-function App() {
+const Frame = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='header'>Dropbox supports you and your business at every stage</h1>
+    
+    <div className="frame">
+      <Section
+        title="Store and protect your files"
+        description="Starting at 3 TB of team storage and backup, with security features like file recovery, password protection, watermarking, and viewer history."
+        imgSrc="Component 1.svg"
+      />
+      <Section
+        title="Share your content"
+        description="Forget email attachments. With Dropbox, you can deliver large files and share trackable links with real-time document analytics."
+        imgSrc="Component 2.svg"
+      />
+      <Section
+        title="Collaborate on your work"
+        description="Reduce meetings with Capture and use Replay to streamline feedback and approval processes on video projects."
+        imgSrc="Component 3.svg"
+      />
+      <Section
+        title="Manage your business"
+        description="Make it easy to prepare, send, and eSign your most important contracts with eSignature templates."
+        imgSrc="Component 4.svg"
+      />
+    </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Frame;
